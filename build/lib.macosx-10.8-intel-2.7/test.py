e3461f68345a5142e7b1688601ae073099d22461
Alban D <nabla.c0d3@gmail.com>
2013-05-12 00:38:35 +0200

    Add tests for all the new stuff

--- build/lib.macosx-10.8-intel-2.7/test.py
+++ build/lib.macosx-10.8-intel-2.7/test.py
@@ -5,10 +5,22 @@ import nassl
 testCTX = nassl.SSL_CTX(nassl.SSLV23)
 testCTX.set_verify(nassl.SSL_VERIFY_NONE)
 
+testCTX.set_cipher_list("LOW")
+testCTX.load_verify_locations("lol")
+
 testSSL = nassl.SSL(testCTX)
 print testSSL
 
+
+testSSL.set_verify(nassl.SSL_VERIFY_NONE)
+testSSL.set_tlsext_host_name("www.lol.com")
+
 testSSL.do_handshake()
+#certpointer = testSSL.get_peer_certificate()
+#print certpointer
+#cert = nassl.X509(certpointer)
+#print cert.as_text()
+
 testSSL.write("GET / HTTP/1.0\n\n")
 
 
@@ -16,6 +28,10 @@ print testSSL.read(2048)
 while testSSL.pending():
 	print testSSL.read(2048)
 
+print testSSL.get_secure_renegotiation_support()
+
+
+print testSSL.get_current_compression_name()
 
 
 # Wrong ssl version
