35f345e30c19ff1e1dda19258423aab952bba592
Alban D <nabla.c0d3@gmail.com>
2013-06-25 14:40:44 -0700

    More X509 tests

--- test/X509_Tests.py
+++ test/X509_Tests.py
@@ -4,10 +4,15 @@ import socket
 from SslClient import SslClient
 
 
-class SSL_CTX_Tests(unittest.TestCase):
+class X509_Tests_Tests(unittest.TestCase):
+
+    def test_new_bad(self):
+        self.assertRaises(NotImplementedError, nassl.X509, (None))
+
+
+class X509_Tests_Online(unittest.TestCase):
 
     def setUp(self):
-        # TODO: Juste create a cert somehow instead of connecting to google
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(5)
         sock.connect(("www.google.com", 443))
@@ -31,6 +36,19 @@ class SSL_CTX_Tests(unittest.TestCase):
 
     def test_get_notAfter(self):
         self.assertIsNotNone(self.cert.get_notAfter())
+
+
+    def test_digest(self):
+        self.assertIsNotNone(self.cert.digest())
+
+
+    def test_as_pem(self):
+        self.assertIsNotNone(self.cert.as_pem())
+
+
+    def test_get_ext_count(self):
+        self.assertIsNotNone(self.cert.get_ext_count())
+
 
 
 def main():
