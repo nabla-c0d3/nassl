ecef71b00f516d70aa7768f35982783983cd5239
Alban D <nabla.c0d3@gmail.com>
2013-07-01 22:02:07 -0700

    Move C classes to _nassl

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup, Extension
 setup(name="_nassl", version="1.0",
       ext_modules=[Extension(
       	"_nassl", 
-      	["src/nassl.c", "src/nassl_SSL_CTX.c", "src/nassl_SSL.c", "src/nassl_X509.c", "src/nassl_errors.c", "src/nassl_BIO.c", "src/nassl_X509_EXTENSION.c", "src/nassl_X509_NAME_ENTRY.c"], 
+      	["src/_nassl/nassl.c", "src/_nassl/nassl_SSL_CTX.c", "src/_nassl/nassl_SSL.c", "src/_nassl/nassl_X509.c", "src/_nassl/nassl_errors.c", "src/_nassl/nassl_BIO.c", "src/_nassl/nassl_X509_EXTENSION.c", "src/_nassl/nassl_X509_NAME_ENTRY.c"], 
       	include_dirs = ['/Users/nabla/Downloads/openssl-1.0.1e/include/'],
       	extra_objects=['./libcrypto.a', './libssl.a'])])
