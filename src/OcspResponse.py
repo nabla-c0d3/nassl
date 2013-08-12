6d616796d7f8a4a118f6502fb9de0f4c91ba58d2
Alban D <nabla.c0d3@gmail.com>
2013-08-12 13:18:14 -0700

    Add OcspResponse.as_dict()

--- src/OcspResponse.py
+++ src/OcspResponse.py
@@ -17,3 +17,40 @@ class OcspResponse:
         
     def verify(self, verifyLocations):
         return self._ocspResp.basic_verify(verifyLocations)
+
+
+    def as_dict(self):
+        # For now we just parse OpenSSL's text output and make a lot of assumptions
+        respDict = \
+            {'responseStatus': self._get_value_from_text_output_no_p('OCSP Response Status:'),
+             'version' : self._get_value_from_text_output_no_p('Version:'),
+             'responseType': self._get_value_from_text_output('Response Type:'),
+             'responderID': self._get_value_from_text_output('Responder Id:'),
+             'producedAt': self._get_value_from_text_output('Produced At:'),
+             'responses': {
+                'certID': { 
+                    'hashAlgorithm': self._get_value_from_text_output('Hash Algorithm:'),
+                    'issuerNameHash': self._get_value_from_text_output('Issuer Name Hash:'),
+                    'issuerKeyHash': self._get_value_from_text_output('Issuer Key Hash:'),
+                    'serialNumber': self._get_value_from_text_output('Serial Number:')
+                    },
+                'certStatus': self._get_value_from_text_output('Cert Status:'),
+                'thisUpdate': self._get_value_from_text_output('This Update:'),
+                'nextUpdate': self._get_value_from_text_output('Next Update:')
+                }
+            }
+        
+        return respDict
+
+
+    def _get_value_from_text_output(self, key):
+        value = self._ocspResp.as_text().split(key)
+        return value[1].split('\n')[0].strip()
+
+
+    def _get_value_from_text_output_no_p(self, key):
+        value = self._ocspResp.as_text().split(key)
+        value = value[1].split('\n')[0].strip()
+        return value.split('(')[0].strip()
+
+
