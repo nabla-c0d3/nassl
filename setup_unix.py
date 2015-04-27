3a79fae92cd25dd223212916833e9377297fb376
Alban Diquet <nabla.c0d3@gmail.com>
2015-04-26 23:41:01 -0700

    Properly link OpenSSL and Zlib statically
    Fixes issue #25 and issue #27

--- setup_unix.py
+++ setup_unix.py
@@ -9,18 +9,13 @@ from buildAll_unix import OPENSSL_INSTALL_DIR
 
 extra_compile_args = ['-Wall', '-Wno-deprecated-declarations']
 
-if platform == 'darwin': # Workaround for Clang 3.4
-    # add as the element of an array rather than a string, py 2.7.5 
-    extra_compile_args += ['-Wno-error=unused-command-line-argument-hard-error-in-future']
-
 
 # Add arguments specific to Unix builds
 unix_ext_args = NASSL_EXT_SETUP.copy()
 unix_ext_args.update({
     'include_dirs' : [OPENSSL_INSTALL_DIR + '/include'],
     'extra_compile_args' : extra_compile_args,
-    'library_dirs' : [OPENSSL_DIR, ZLIB_DIR],
-    'libraries' : ['ssl', 'crypto']})
+    'extra_objects': [OPENSSL_INSTALL_DIR + '/lib/libssl.a', OPENSSL_INSTALL_DIR + '/lib/libcrypto.a', ZLIB_DIR + '/libz.a']})
 
 
 unix_setup = NASSL_SETUP.copy()
