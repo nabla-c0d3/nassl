596ea71bb198db40027cd5e2079e3d07bfa9a743
Alban Diquet <nabla.c0d3@gmail.com>
2016-01-22 23:09:03 -0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -31,13 +31,13 @@ extra_compile_args = ['-Wall', '-Wno-deprecated-declarations']
 # Add arguments specific to Unix builds
 unix_ext_args = NASSL_EXT_SETUP.copy()
 unix_ext_args.update({
-    'include_dirs': [OPENSSL_INSTALL_DIR + '/include'],
+    'include_dirs': [join('bin', 'openssl', 'include')],
     'extra_compile_args': extra_compile_args,
-    'extra_objects': [OPENSSL_INSTALL_DIR + '/lib/libssl.a', OPENSSL_INSTALL_DIR + '/lib/libcrypto.a',
-                      ZLIB_DIR + '/libz.a']})
+    'extra_objects': [join(OPENSSL_INSTALL_DIR, 'libssl.a'), join(OPENSSL_INSTALL_DIR, 'libcrypto.a')]})
+
 
 unix_setup = NASSL_SETUP.copy()
 unix_setup.update({
-    'ext_modules': [Extension(**unix_ext_args)]})
+    'ext_modules': [Extension(**unix_ext_args)] })
 
 setup(**unix_setup)
