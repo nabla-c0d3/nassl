9934b5e6446394fc5e4a8585f5260a13c24d77f4
Alban Diquet <nabla.c0d3@gmail.com>
2016-01-23 15:47:27 -0800

    Improve setup and build scripts

--- buildAll_unix.py
+++ buildAll_unix.py
@@ -1,37 +1,54 @@
 #!/usr/bin/python2.7
 import os
 import shutil
-from platform import architecture
-from sys import platform
+from setup import OPENSSL_LIB_INSTALL_PATH, CURRENT_PLATFORM, SupportedPlatformEnum, OPENSSL_HEADERS_INSTALL_PATH
+from os import getcwd
 from os.path import join
-import distutils.util
-from buildAll_config import OPENSSL_CONF_CMD, BUILD_DIR, PY_VERSION, OPENSSL_DIR, ZLIB_DIR, TEST_DIR, perform_build_task
+import subprocess
 
+# Base script to build Zlib, OpenSSL and nassl from scratch
+# The build scripts expect the OpenSSL and Zlib src packages to be in nassl's root folder
+# Warning: use a fresh Zlib src tree on Windows or build will fail ie. do not use the same Zlib src folder for Windows
+# and Unix build
+ZLIB_PATH = join(getcwd(), 'zlib-1.2.8')
+OPENSSL_PATH = join(getcwd(), 'openssl')
+OPENSSL_CONF_CMD = (
+    'perl Configure {target} --prefix={install_path} zlib no-zlib-dynamic no-shared enable-rc5 enable-md2 enable-gost '
+    'enable-cast enable-idea enable-ripemd enable-mdc2 --with-zlib-include={zlib_path} '
+    '--with-zlib-lib={zlib_install_path} {extra_args}'
+).format
 
-NASSL_INSTALL_DIR = ''
 
 # I've only tried building nassl on OS X 64 bits and Linux 32/64 bits
 # This will fail if you're cross-compiling
-if architecture()[0] == '64bit':
-    if platform == 'darwin':
-        OPENSSL_TARGET = 'darwin64-x86_64-cc'
-        NASSL_INSTALL_DIR = join('build', 'lib.' + distutils.util.get_platform() + '-' + PY_VERSION, 'nassl')
-        OPENSSL_INSTALL_DIR = join(BUILD_DIR, 'openssl', 'darwin64')
+ZLIB_INSTALL_PATH = ZLIB_PATH
+OPENSSL_BUILD_EXTRA_ARGS = ' -fPIC'
 
-    elif platform == 'linux2':
-        OPENSSL_TARGET = 'linux-x86_64'
-        NASSL_INSTALL_DIR = join('build', 'lib.linux-x86_64-' + PY_VERSION + '/')
-        OPENSSL_INSTALL_DIR = join(BUILD_DIR, 'openssl', 'linux64')
+if CURRENT_PLATFORM == SupportedPlatformEnum.OSX_64:
+    OPENSSL_TARGET = 'darwin64-x86_64-cc'
 
-elif architecture()[0] == '32bit':
-    if platform == 'linux2':
-        OPENSSL_TARGET = 'linux-elf'
-        NASSL_INSTALL_DIR = join('build', 'lib.linux-i686-' + PY_VERSION + '/')
-        OPENSSL_INSTALL_DIR = join(BUILD_DIR, 'openssl', 'linux32')
+elif CURRENT_PLATFORM == SupportedPlatformEnum.LINUX_64:
+    OPENSSL_TARGET = 'linux-x86_64'
+
+elif CURRENT_PLATFORM == SupportedPlatformEnum.LINUX_32:
+    OPENSSL_TARGET = 'linux-elf'
+
+elif CURRENT_PLATFORM == SupportedPlatformEnum.WINDOWS_32:
+    OPENSSL_TARGET = 'VC-WIN32'
+    ZLIB_INSTALL_PATH = ZLIB_PATH + '\\contrib\\vstudio\\vc9\\x86\\ZlibStatRelease\\zlibstat.lib'
+    OPENSSL_BUILD_EXTRA_ARGS = ' no-asm -DZLIB_WINAPI'  # *hate* zlib
+
+elif CURRENT_PLATFORM == SupportedPlatformEnum.WINDOWS_64:
+    OPENSSL_TARGET = 'VC-WIN64A'
+    ZLIB_INSTALL_PATH = ZLIB_PATH + '\\contrib\\vstudio\\vc9\\x64\\ZlibStatRelease\\zlibstat.lib'
+    OPENSSL_BUILD_EXTRA_ARGS = ' -DZLIB_WINAPI'
 
 
-if NASSL_INSTALL_DIR == '':
-    raise Exception('Plaftorm ' + platform + ' ' + architecture()[0] + ' not supported.')
+
+def perform_build_task(title, commandsDict, cwd=None):
+    print '===BUILDING {0}==='.format(title)
+    for command in commandsDict:
+        subprocess.check_call(command, shell=True, cwd=cwd)
 
 
 def main():
@@ -41,40 +58,41 @@ def main():
         'make clean',
         'make'
     ]
-    perform_build_task('ZLIB', ZLIB_BUILD_TASKS, ZLIB_DIR)
+    perform_build_task('ZLIB', ZLIB_BUILD_TASKS, ZLIB_PATH)
 
 
     # Build OpenSSL
     OPENSSL_BUILD_TASKS = [
-        OPENSSL_CONF_CMD(OPENSSL_TARGET, OPENSSL_INSTALL_DIR, ZLIB_DIR,  ZLIB_DIR) + ' -fPIC',
+        OPENSSL_CONF_CMD(target=OPENSSL_TARGET, install_path=OPENSSL_LIB_INSTALL_PATH, zlib_path=ZLIB_PATH,
+                         zlib_install_path=ZLIB_INSTALL_PATH, extra_args=OPENSSL_BUILD_EXTRA_ARGS),
         'make clean',
         #'make depend',  # This makes building with Clang on OS X fail
         'make',
         'make test',
         'make install_sw',  # Don't build documentation, else will fail on Debian
     ]
-    perform_build_task('OPENSSL', OPENSSL_BUILD_TASKS, OPENSSL_DIR)
+    perform_build_task('OPENSSL', OPENSSL_BUILD_TASKS, OPENSSL_PATH)
 
     # Setup the OpenSSL folder
     # Move the static libraries to the root folder
-    shutil.copy(join(OPENSSL_INSTALL_DIR, 'lib', 'libcrypto.a'), OPENSSL_INSTALL_DIR)
-    shutil.copy(join(OPENSSL_INSTALL_DIR, 'lib', 'libssl.a'), OPENSSL_INSTALL_DIR)
+    shutil.copy(join(OPENSSL_LIB_INSTALL_PATH, 'lib', 'libcrypto.a'), OPENSSL_LIB_INSTALL_PATH)
+    shutil.copy(join(OPENSSL_LIB_INSTALL_PATH, 'lib', 'libssl.a'), OPENSSL_LIB_INSTALL_PATH)
 
-    # Move the headers
-    shutil.rmtree(join(BUILD_DIR, 'openssl', 'include'))
-    shutil.move(join(OPENSSL_INSTALL_DIR, 'include'), join(BUILD_DIR, 'openssl'))
+    # Move the header to ./bin/openssl
+    shutil.rmtree(OPENSSL_HEADERS_INSTALL_PATH)
+    shutil.move(join(OPENSSL_LIB_INSTALL_PATH, 'include'), OPENSSL_HEADERS_INSTALL_PATH)
 
     # Copy some internal headers for accessing EDH and ECDH parameters
-    HEADERS_PATH = join(BUILD_DIR, 'openssl', 'include', 'openssl-internal')
-    if not os.path.isdir(HEADERS_PATH):
-        os.makedirs(HEADERS_PATH)
-    shutil.copy(join(OPENSSL_DIR, 'e_os.h'), HEADERS_PATH)
-    shutil.copy(join(OPENSSL_DIR, 'ssl', 'ssl_locl.h'), HEADERS_PATH)
+    INTERNAL_HEADERS_INSTALL_PATH = join(OPENSSL_HEADERS_INSTALL_PATH, 'openssl-internal')
+    if not os.path.isdir(INTERNAL_HEADERS_INSTALL_PATH):
+        os.makedirs(INTERNAL_HEADERS_INSTALL_PATH)
+    shutil.copy(join(OPENSSL_PATH, 'e_os.h'), INTERNAL_HEADERS_INSTALL_PATH)
+    shutil.copy(join(OPENSSL_PATH, 'ssl', 'ssl_locl.h'), INTERNAL_HEADERS_INSTALL_PATH)
 
     # Erase everything else
-    shutil.rmtree(join(OPENSSL_INSTALL_DIR, 'lib'))
-    shutil.rmtree(join(OPENSSL_INSTALL_DIR, 'bin'))
-    shutil.rmtree(join(OPENSSL_INSTALL_DIR, 'ssl'))
+    shutil.rmtree(join(OPENSSL_LIB_INSTALL_PATH, 'lib'))
+    shutil.rmtree(join(OPENSSL_LIB_INSTALL_PATH, 'bin'))
+    shutil.rmtree(join(OPENSSL_LIB_INSTALL_PATH, 'ssl'))
 
 
     # Build nassl
@@ -83,7 +101,7 @@ def main():
 
     # Test nassl
     NASSL_TEST_TASKS = ['python2.7 run_tests.py']
-    perform_build_task('NASSL Tests', NASSL_TEST_TASKS, TEST_DIR)
+    perform_build_task('NASSL Tests', NASSL_TEST_TASKS)
 
 
     print '=== All Done! ==='
--- setup.py
+++ setup.py
@@ -1,10 +1,47 @@
 #!/usr/bin/python2.7
 from distutils.core import setup, Extension
-
+from os import getcwd
 from os.path import join
-
-from buildAll_unix import OPENSSL_INSTALL_DIR
+from platform import architecture
+from sys import platform
 from nassl import __author__, __version__
+
+
+class SupportedPlatformEnum:
+    """Platforms supported by nassl.
+    """
+    OSX_64 = 1
+    LINUX_64 = 2
+    LINUX_32 = 3
+    WINDOWS_32 = 4
+    WINDOWS_64 = 5
+
+
+CURRENT_PLATFORM = None
+if architecture()[0] == '64bit':
+    if platform == 'darwin':
+        CURRENT_PLATFORM = SupportedPlatformEnum.OSX_64
+    elif platform == 'linux2':
+        CURRENT_PLATFORM = SupportedPlatformEnum.LINUX_64
+elif architecture()[0] == '32bit':
+    if platform == 'linux2':
+        CURRENT_PLATFORM = SupportedPlatformEnum.LINUX_32
+# TODO: Add Windows
+
+
+
+OPENSSL_INSTALL_PATH_DICT = {
+    SupportedPlatformEnum.OSX_64: join(getcwd(), 'bin', 'openssl', 'darwin64'),
+    SupportedPlatformEnum.LINUX_64: join(getcwd(), 'bin', 'openssl', 'linux64'),
+    SupportedPlatformEnum.LINUX_32: join(getcwd(), 'bin', 'openssl', 'linux32'),
+    SupportedPlatformEnum.WINDOWS_32: join(getcwd(), 'bin', 'openssl', 'windows32'),
+    SupportedPlatformEnum.WINDOWS_64: join(getcwd(), 'bin', 'openssl', 'windows64'),
+}
+
+
+OPENSSL_LIB_INSTALL_PATH = OPENSSL_INSTALL_PATH_DICT[CURRENT_PLATFORM]
+OPENSSL_HEADERS_INSTALL_PATH = join('bin', 'openssl', 'include')
+
 
 NASSL_SETUP = {
     'name': "nassl",
@@ -12,10 +49,10 @@ NASSL_SETUP = {
     'package_dir': {'nassl': 'nassl'},
     'py_modules': ['nassl.__init__', 'nassl.SslClient', 'nassl.DebugSslClient', 'nassl.X509Certificate',
                    'nassl.OcspResponse'],
-    'description': 'OpenSSL wrapper for SSLyze',
+    'description': 'Experimental OpenSSL wrapper for SSLyze.',
     'author': __author__,
     'author_email': 'nabla.c0d3@gmail.com',
-    'url': 'https://github.com/nabla-c0d3/nassl'
+    'url': 'https://github.com/nabla-c0d3/nassl',
 }
 
 NASSL_EXT_SETUP = {
@@ -23,21 +60,22 @@ NASSL_EXT_SETUP = {
     'sources': ["nassl/_nassl/nassl.c", "nassl/_nassl/nassl_SSL_CTX.c", "nassl/_nassl/nassl_SSL.c",
                 "nassl/_nassl/nassl_X509.c", "nassl/_nassl/nassl_errors.c", "nassl/_nassl/nassl_BIO.c",
                 "nassl/_nassl/nassl_X509_EXTENSION.c", "nassl/_nassl/nassl_X509_NAME_ENTRY.c",
-                "nassl/_nassl/nassl_SSL_SESSION.c", "nassl/_nassl/openssl_utils.c", "nassl/_nassl/nassl_OCSP_RESPONSE.c"]
+                "nassl/_nassl/nassl_SSL_SESSION.c", "nassl/_nassl/openssl_utils.c",
+                "nassl/_nassl/nassl_OCSP_RESPONSE.c"]
 }
-
-extra_compile_args = ['-Wall', '-Wno-deprecated-declarations']
 
 # Add arguments specific to Unix builds
 unix_ext_args = NASSL_EXT_SETUP.copy()
 unix_ext_args.update({
-    'include_dirs': [join('bin', 'openssl', 'include')],
-    'extra_compile_args': extra_compile_args,
-    'extra_objects': [join(OPENSSL_INSTALL_DIR, 'libssl.a'), join(OPENSSL_INSTALL_DIR, 'libcrypto.a')]})
+    'include_dirs': [OPENSSL_HEADERS_INSTALL_PATH, join('nassl', '_nassl')],
+    'extra_compile_args': ['-Wall'],
+    'extra_objects': [join(OPENSSL_LIB_INSTALL_PATH, 'libssl.a'), join(OPENSSL_LIB_INSTALL_PATH, 'libcrypto.a')]
+})
 
 
 unix_setup = NASSL_SETUP.copy()
-unix_setup.update({
-    'ext_modules': [Extension(**unix_ext_args)] })
+unix_setup.update({'ext_modules': [Extension(**unix_ext_args)]})
 
-setup(**unix_setup)
+
+if __name__ == "__main__":
+    setup(**unix_setup)
