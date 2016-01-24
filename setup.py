10b6198edfb569796953e488cc429fef55a00a71
Alban Diquet <nabla.c0d3@gmail.com>
2016-01-23 16:09:19 -0800

    Add files for adding nassl to PyPi

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python2.7
 from distutils.core import setup, Extension
-from os import getcwd
 from os.path import join
 from platform import architecture
 from sys import platform
@@ -31,11 +30,11 @@ elif architecture()[0] == '32bit':
 
 
 OPENSSL_INSTALL_PATH_DICT = {
-    SupportedPlatformEnum.OSX_64: join(getcwd(), 'bin', 'openssl', 'darwin64'),
-    SupportedPlatformEnum.LINUX_64: join(getcwd(), 'bin', 'openssl', 'linux64'),
-    SupportedPlatformEnum.LINUX_32: join(getcwd(), 'bin', 'openssl', 'linux32'),
-    SupportedPlatformEnum.WINDOWS_32: join(getcwd(), 'bin', 'openssl', 'windows32'),
-    SupportedPlatformEnum.WINDOWS_64: join(getcwd(), 'bin', 'openssl', 'windows64'),
+    SupportedPlatformEnum.OSX_64: join('bin', 'openssl', 'darwin64'),
+    SupportedPlatformEnum.LINUX_64: join('bin', 'openssl', 'linux64'),
+    SupportedPlatformEnum.LINUX_32: join('bin', 'openssl', 'linux32'),
+    SupportedPlatformEnum.WINDOWS_32: join('bin', 'openssl', 'windows32'),
+    SupportedPlatformEnum.WINDOWS_64: join('bin', 'openssl', 'windows64'),
 }
 
 
@@ -61,7 +60,7 @@ NASSL_EXT_SETUP = {
                 "nassl/_nassl/nassl_X509.c", "nassl/_nassl/nassl_errors.c", "nassl/_nassl/nassl_BIO.c",
                 "nassl/_nassl/nassl_X509_EXTENSION.c", "nassl/_nassl/nassl_X509_NAME_ENTRY.c",
                 "nassl/_nassl/nassl_SSL_SESSION.c", "nassl/_nassl/openssl_utils.c",
-                "nassl/_nassl/nassl_OCSP_RESPONSE.c"]
+                "nassl/_nassl/nassl_OCSP_RESPONSE.c"],
 }
 
 # Add arguments specific to Unix builds
