c94b8a7cad486830204dbea244469766b6f4ee21
Alban Diquet <nabla.c0d3@gmail.com>
2016-12-31 13:12:03 -0800

    Revert debug change

--- sample_client.py
+++ sample_client.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python2.7
-import pprint
 import socket
 from nassl import TLSV1, SSL_VERIFY_NONE
 from nassl.debug_ssl_client import DebugSslClient
@@ -7,15 +6,14 @@ from nassl.debug_ssl_client import DebugSslClient
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.settimeout(5)
-sock.connect(("www.python.org", 443))
+sock.connect(("www.google.com", 443))
 
 ssl_client = DebugSslClient(ssl_version=TLSV1, sock=sock, ssl_verify=SSL_VERIFY_NONE)
 ssl_client.do_handshake()
 
 print 'Certificate chain'
 for cert in ssl_client.get_peer_cert_chain():
-    pprint.pprint(cert.as_dict())
-    break
+    print cert.as_dict()['subject']['commonName']
 
 print '\nCipher suite'
 print ssl_client.get_current_cipher_name()
