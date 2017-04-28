b1a340fde5389a9a6ba6c3e8d669946304adc6df
Alban Diquet <nabla.c0d3@gmail.com>
2017-04-27 21:24:29 -0700

    Fix tests

--- run_tests.py
+++ run_tests.py
@@ -11,7 +11,7 @@ import unittest
 
 def main():
 
-    suite = unittest.loader.TestLoader().discover('tests', pattern='*_Tests.py')
+    suite = unittest.loader.TestLoader().discover('tests', pattern='*')
     result = unittest.TextTestRunner(verbosity=2).run(suite)
 
     exit_code = 0 if result.wasSuccessful() else 1
