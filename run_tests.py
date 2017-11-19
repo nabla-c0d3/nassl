dbab72b9ca559c129d6c81322e23dbdd46789a7e
Alban Diquet <nabla.c0d3@gmail.com>
2017-11-19 10:39:05 -0800

    Tweak pattern

--- run_tests.py
+++ run_tests.py
@@ -9,7 +9,7 @@ import unittest
 
 def main():
 
-    suite = unittest.loader.TestLoader().discover('tests', pattern='*_Tests.py')
+    suite = unittest.loader.TestLoader().discover('tests', pattern='*_tests.py')
     result = unittest.TextTestRunner(verbosity=2).run(suite)
 
     exit_code = 0 if result.wasSuccessful() else 1
