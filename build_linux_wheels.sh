f0f33cda2d94f367092eed195d6c03bc0f77ca60
Alban Diquet <nabla.c0d3@gmail.com>
2018-03-04 20:25:41 -0800

    Modify docker script to build everything from scratch

--- build_linux_wheels.sh
+++ build_linux_wheels.sh
@@ -2,6 +2,21 @@
 set -e -x
 
 # docker run --rm -v D:\GitHub\nassl\:/io quay.io/pypa/manylinux1_i686 bash /io/build_linux_wheels.sh
+# docker run --rm -v D:\GitHub\nassl\:/io quay.io/pypa/manylinux1_x86_64 bash /io/build_linux_wheels.sh
+
+# Update perl as OpenSSL 1.1.x requires 5.10; there's probably a better/Docker way to do this
+wget http://www.cpan.org/src/perl-5.10.1.tar.gz
+tar -xzf perl-5.10.1.tar.gz
+cd perl-5.10.1
+./Configure -des -Dprefix=$HOME/localperl
+make
+make install
+export PATH=$HOME/localperl/bin:$PATH
+
+# Build Zlib, OpenSSL
+cd ../io
+"/opt/python/cp36-cp36m/bin/python" build_from_scratch.py
+cd ..
 
 # Compile wheels
 for PYBIN in "cp27-cp27m" "cp34-cp34m" "cp35-cp35m" "cp36-cp36m"; do
