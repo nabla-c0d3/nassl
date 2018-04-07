5252c61c7eb5c4a95c5b1e4bee862f381237ec2e
Alban Diquet <nabla.c0d3@gmail.com>
2018-04-07 11:39:05 -0700

    Fix Python 2 wheel

--- build_linux_wheels.sh
+++ build_linux_wheels.sh
@@ -16,7 +16,7 @@ cd ../io
 cd ..
 
 # Compile wheels
-for PYBIN in "cp27-cp27m" "cp34-cp34m" "cp35-cp35m" "cp36-cp36m"; do
+for PYBIN in "cp27-cp27mu" "cp34-cp34m" "cp35-cp35m" "cp36-cp36m"; do
     "/opt/python/${PYBIN}/bin/pip" install -r /io/requirements.txt
     "/opt/python/${PYBIN}/bin/pip" wheel /io/ -w wheelhouse/
 done
