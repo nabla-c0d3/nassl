c1ca5c06fa947837a59e4e82db5bd1234a770e46
Alban Diquet <nabla.c0d3@gmail.com>
2019-12-08 16:10:47 -0800

    Fix OCSP test

--- tests/ocsp_response_test.py
+++ tests/ocsp_response_test.py
@@ -128,7 +128,7 @@ class TestCommonOcspResponseOnline:
         # Given a website that support OCSP stapling
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(5)
-        sock.connect(("www.cloudflare.com", 443))
+        sock.connect(("www.digitalocean.com", 443))
 
         ssl_client = ssl_client_cls(underlying_socket=sock, ssl_verify=OpenSslVerifyEnum.NONE)
         ssl_client.set_tlsext_status_ocsp()
