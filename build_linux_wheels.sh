832ad79d85ffa884ea0f4c207542cba1fd48c48a
Alban Diquet <nabla.c0d3@gmail.com>
2020-03-15 22:22:09 -0700

    Tweak docker builds

--- build_linux_wheels.sh
+++ build_linux_wheels.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e -x
 
-# Update perl as OpenSSL 1.1.x requires 5.10; there's probably a better/Docker way to do this
-curl http://www.cpan.org/src/perl-5.10.1.tar.gz -o perl-5.10.1.tar.gz
-tar -xzf perl-5.10.1.tar.gz
-cd perl-5.10.1
-./Configure -des -Dprefix=$HOME/localperl
-make
-make install
-export PATH=$HOME/localperl/bin:$PATH
-
 
 cd /io
 
@@ -20,17 +11,13 @@ cd /io
 "/opt/python/cp37-cp37m/bin/pipenv" run invoke build.legacy-openssl
 "/opt/python/cp37-cp37m/bin/pipenv" run invoke build.modern-openssl
 
-# Create a requirements file as we won't use pipenv after this point
-"/opt/python/cp37-cp37m/bin/pipenv" lock -r --dev > requirements.txt
-
 # Now build the Python extension and wheel
 for PYBIN in "cp37-cp37m" "cp38-cp38"; do
     "/opt/python/${PYBIN}/bin/python" setup.py clean --all
     "/opt/python/${PYBIN}/bin/python" setup.py build_ext -i
-    "/opt/python/${PYBIN}/bin/pip" install -r requirements.txt
 
     # Compile wheels
-    "/opt/python/${PYBIN}/bin/pip" wheel /io/ -w wheelhouse/
+    "/opt/python/${PYBIN}/bin/pip" wheel /io -w wheelhouse/
 done
 
 
