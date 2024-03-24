b08d317e6fee7540d617bf798d3fdd918001bee2
Alban Diquet <ad@datatheorem.io>
2024-03-24 18:18:24 +0100

    Add Python 3.12

--- build_macos_arm64_wheels.sh
+++ build_macos_arm64_wheels.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Python 3.9+ arm64 is required
-python -m pip install "cibuildwheel<2.13"
+python -m pip install "cibuildwheel>=2.17,<2.18"
 
 export CIBW_ARCHS_MACOS="arm64"
 export CIBW_BEFORE_ALL='python -m pip install invoke && invoke build.deps'
 export CIBW_BEFORE_BUILD='python -m pip install invoke && invoke build.nassl'
-export CIBW_BUILD='cp39-* cp310-* cp311-*'
+export CIBW_BUILD='cp39-* cp310-* cp311-* cp312-*'
 export CIBW_TEST_COMMAND='python -m pytest {project}/tests'
 export CIBW_TEST_REQUIRES="pytest"
 
