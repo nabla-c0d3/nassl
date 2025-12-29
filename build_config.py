5d0d2f4c05db0a190c2afce9853276873026e3c8
Alban D. <nabla.c0d3@gmail.com>
2025-12-29 15:35:18 +0100

    Revert "Update zlib"
    This reverts commit 51e807caabd3e94c53422d9ac992e3d32851d876.

--- build_config.py
+++ build_config.py
@@ -365,11 +365,11 @@ class ModernOpenSslBuildConfig(OpenSslBuildConfig):
 class ZlibBuildConfig(BuildConfig):
     @property
     def src_tar_gz_url(self) -> str:
-        return "https://www.zlib.net/fossils/zlib-1.3.1.tar.gz"
+        return "https://www.zlib.net/fossils/zlib-1.2.13.tar.gz"
 
     @property
     def src_path(self) -> Path:
-        return _DEPS_PATH / "zlib-1.3.1"
+        return _DEPS_PATH / "zlib-1.2.13"
 
     def build(self, ctx: "Context") -> None:
         if self.platform in [
